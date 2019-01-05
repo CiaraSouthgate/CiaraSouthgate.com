e915f02f17dbd60811bfdda76ab60890594f9584
Ciara Southgate <ciarajsouthgate@gmail.com>
2019-01-05 00:06:00 -0800

    added wizard to projects, animated switching between them

--- js/index.js
+++ js/index.js
@@ -1,15 +1,13 @@
-$(document).ready(function () {
-  
-  $("#myName").hover(
-    function() {
-      $("#popup").css("animation", "fade-in .5s forwards");
-      console.log("hover");
-    },
-    function() {
-      $("#popup").css("animation", "fade-out .5s forwards");
-    }
-  );
-  
+$("#myName").hover(
+  function () {
+    $("#popup").css("animation", "fade-in .5s forwards");
+    console.log("hover");
+  },
+  function () {
+    $("#popup").css("animation", "fade-out .5s forwards");
+  }
+);
+
 //  $("#worked").hover(
 //    function() {
 //      $("#worked").html("played");
@@ -19,61 +17,129 @@ $(document).ready(function () {
 //    }
 //  )
 
-  var animateLogos = function () {
-    var elems;
-    var windowHeight;
+var animateLogos = function () {
+  var elems, windowHeight;
 
-    function init() {
-      elems = $(".hidden");
-      windowHeight = $(window).height();
-      addEventHandlers();
-      checkPosition();
-    }
+  function init() {
+    elems = $(".hidden");
+    windowHeight = $(window).height();
+    addEventHandlers();
+    checkPosition();
+  }
 
-    function addEventHandlers() {
-      window.addEventListener("scroll", checkPosition);
-      window.addEventListener("resize", init);
-    }
+  function addEventHandlers() {
+    window.addEventListener("scroll", checkPosition);
+    window.addEventListener("resize", init);
+  }
 
-    function checkPosition() {
-      for (var i = 0; i < elems.length; i++) {
-        var positionFromTop = elems[i].getBoundingClientRect().top;
-        if (positionFromTop - windowHeight <= -60) {
-          elems[i].className = elems[i].className.replace(
-            "hidden",
-            "slide-in-element"
-          );
-        }
+  function checkPosition() {
+    for (var i = 0; i < elems.length; i++) {
+      var positionFromTop = elems[i].getBoundingClientRect().top;
+      if (positionFromTop - windowHeight <= -60) {
+        elems[i].className = elems[i].className.replace(
+          "hidden",
+          "slide-in-element"
+        );
       }
     }
-    return {
-      init: init
-    };
+  }
+  return {
+    init: init
   };
-  animateLogos().init();
+};
+animateLogos().init();
 
-  window.addEventListener("hashchange", function () {
-    window.scrollTo(window.scrollX, window.scrollY - 90);
-  });
-  
-  var descriptionVisible = false;
-  
-  $("#next").click(function() {
-    if ($(window).width() < 768 && !descriptionVisible) {
-      $("#mmDescription").css("animation", "appear 1s forwards");
-      $("#previous").css("display", "initial");
-      $("#next").css("display", "none");
+window.addEventListener("hashchange", function () {
+  window.scrollTo(window.scrollX, window.scrollY - 90);
+});
+
+var projects = $("#gallery").find(".projectDiv");
+var descriptionVisible = false;
+var projectVisible = 0;
+
+$("#next").click(function() {
+  var current = projects[projectVisible];
+  var next = projects[projectVisible + 1];
+  if ($(window).width() < 768) {
+    if (!descriptionVisible) {
+      let description = $(current).find(".description");
+      $(description).css("animation", "appear 1s forwards");
       descriptionVisible = true;
-    }
-  });
-  
-  $("#previous").click(function() {
-    if ($(window).width() < 768 && descriptionVisible) {
-      $("#mmDescription").css("animation", "disappear 1s forwards");
-      $("#previous").css("display", "none");
-      $("#next").css("display", "initial");
+      
+      $("#previous").css("display", "table-cell");
+
+      if (projectVisible + 1 == projects.length) {
+        $("#next").css("display", "none");
+      }
+    } else {
+      $(next).find(".description").css("opacity", "0");
+      $(current).css("animation", "disappear 1s forwards");
+      $(next).css("display", "inline-block");
+      $(next).css("animation", "appear 1s forwards");
+      $(current).css("display", "none");
+      
       descriptionVisible = false;
+      projectVisible++;
     }
-  });
-  
+  } else {
+    $(current).css("animation", "disappear 1s forwards");
+    $(next).css("display", "block");
+    $(next).css("animation", "appear 1s forwards");
+    $(current).css("display", "none");
+      
+    $("#previous").css("display", "table-cell");
+    
+    projectVisible++;
+    
+    if (projectVisible + 1 == projects.length) {
+        $("#next").css("display", "none");
+      }
+  }
+});
+
+$(window).resize(function() {
+  if ($(window).width() >= 768) {
+    var des = $(".projectDiv").find(".description");
+    $(des).css("opacity", "1");
+  }
+});
+
+$("#previous").click(function() {
+  var current = projects[projectVisible];
+  var next = projects[projectVisible - 1];
+  if ($(window).width() < 768) {
+    if (descriptionVisible) {
+      let description = $(current).find(".description");
+      $(description).css("animation", "disappear 1s forwards");
+      descriptionVisible = false;
+      
+      $("#next").css("display", "table-cell");
+      
+      if (projectVisible == 0) {
+        $("#previous").css("display", "none");
+      }
+    } else {
+      $(next).find(".description").css("opacity", "1");
+      $(current).css("animation", "disappear 1s forwards");
+      $(next).css("display", "inline-block");
+      $(next).css("animation", "appear 1s forwards");
+      $(current).css("display", "none");
+      
+      descriptionVisible = true;
+      projectVisible--;
+    }
+  } else {
+    $(current).css("animation", "disappear 1s forwards");
+    $(next).css("display", "block");
+    $(next).css("animation", "appear 1s forwards");
+    $(current).css("display", "none");
+    
+    $("#next").css("display", "table-cell");
+    
+    projectVisible--;
+    
+    if (projectVisible == 0) {
+      $("#previous").css("display", "none");
+    }
+  }
 });
